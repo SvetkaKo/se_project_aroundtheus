517eac6f2f1283c8e0d781e0dde61e2bf7fa2f8b
Svetlana Kovakina <skovakina@gmail.com>
2023-08-30 16:37:28 -0700

    added modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,16 @@ let initialCards = [
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg',
   },
 ];
+
+let btnClose = document.querySelector('.modal__close-button-img');
+let btnEdit = document.querySelector('.profile__btn_type_edit');
+let btnSave = document.querySelector('.modal__save-button');
+
+function modalEdit() {
+  let modal = document.querySelector('.modal');
+  modal.classList.toggle('modal_opened');
+}
+
+btnClose.addEventListener('click', modalEdit);
+btnEdit.addEventListener('click', modalEdit);
+btnSave.addEventListener('click', modalEdit);
